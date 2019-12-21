bf019df742b907bd0dfad8ace24f124e26a54283
hwanpenn <huangpeng@atwasoft.net>
2019-12-22 01:26:02 +0800

    1

--- punch.py
+++ punch.py
@@ -14,7 +14,7 @@ def punch(cmd):
     execute("adb shell input keyevent 26",100)
     execute("adb shell input tap 400 600", 2000)
     execute("adb shell input tap 175 576", 10000)
-    execute("adb shell input text "dingding5277"", 2000)
+    execute("adb shell input text 'dingding5277'", 2000)
     execute("adb shell input tap 352 694", 2000)
     execute("adb shell input tap 400 600", 60000)
     execute("adb shell input keyevent 26", 2000)
