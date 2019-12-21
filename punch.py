add907ef78c767b1e8aac9accb6ef81d3a61497c
hwanpenn <huangpeng@atwasoft.net>
2019-12-22 01:13:38 +0800

    3

--- punch.py
+++ punch.py
@@ -6,6 +6,7 @@ import os
 def exec(cmd,time):
     time.sleep(time)
     print('Job4-startTime:%s' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    str = adbShell.format(cmdStr=cmd)
     print(str)
     os.system(str)
 
@@ -19,6 +20,7 @@ def punch(cmd):
     exec("adb shell input keyevent 26", 2000)
 
 if __name__ == '__main__':
+    print("start-----")
     schedule.every().day.at('1:20').do(punch)
     while True:
         schedule.run_pending()
